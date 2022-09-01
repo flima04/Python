8a736f332b39391becf485d732c09af9552dba38
flima04 <112064779+flima04@users.noreply.github.com>
2022-09-01 05:39:07 -0300

    desafio 02 corrigido

--- desafio002.py
+++ desafio002.py
@@ -1,6 +1,8 @@
 print('Eu posso mostrar a soma entre dois números. Vamos lá?')
 
-num1 = input('Digite o primeiro número')
-num2 = input('Agora, digite o segundo número')
+n1 = int(input('Digite o primeiro número'))
+n2 = int(input('Agora, digite o segundo número'))
 
-print('A soma de', num1, 'e', num2, 'é igual a:', (num1+num2))
+s = n1+n2
+
+print('A soma de {} e {} é igual a: {}' .format(n1, n2, s))
