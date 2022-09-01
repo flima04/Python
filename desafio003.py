bb587bd2b6cfad7601a36baca1f9050305d08226
flima04 <112064779+flima04@users.noreply.github.com>
2022-09-01 05:56:55 -0300

    Update desafio003.py

--- desafio003.py
+++ desafio003.py
@@ -1,5 +1,9 @@
 n = input('Digite algo')
 
-print(type(n))
-print(n.isnumeric())
-print(n.isalpha())
+print('Qual o tipo primitivo?', type(n))
+print('É numérico?', n.isnumeric())
+print('É alfabético?', n.isalpha())
+print('É alfanumérico?', n.isalnum())
+print('Está em caixa baixa?', n.islower())
+print('Está em caixa alta?', n.isupper())
+print('Está capitalizado?', n.istitle())
